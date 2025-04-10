96f95acb5864cd11af952dd1b96b0fd803951ff3
douglas <douglas.arau@hotmail.com>
2025-04-10 13:15:57 -0300

    adicionado funcionalidade para apagar todos os caracteres da calculadora

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 const resultado = document.querySelector(".resultado");
-
 const botoes = document.querySelectorAll("button");
+let expressao = "";
+
+function botaoReset() {
+    resultado.textContent = "";
+} 
+
+
+botoes.forEach((botao) => {
+    botao.addEventListener("click", () => {
+        const valor = botao.textContent;
+        console.log("gay" + valor);
+    })
+});
 
