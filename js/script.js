65cc79e204922867cb0d7f50bcde7da554706e81
douglas <douglas.arau@hotmail.com>
2025-04-10 18:03:01 -0300

    mais ajustes no javascript

--- js/script.js
+++ js/script.js
@@ -1,35 +1,97 @@
-const resultado = document.querySelector(".resultado");
+const resultado = document.getElementById("idResultado");
 const botoes = document.querySelectorAll("button");
 
+let operadoresDisponiveis = ["+","-","*","÷"];
+let operador = "";
+let expressao = "";
+let parte1 = "";
+let parte2 = "";
+
 function resetarBotao() {
-    resultado.textContent = "0";
+    resultado.value = "0";
 }
 
-function ApagarUmCaracterBotao() { 
-    let textoAtual = resultado.innerHTML;
+function apagarUmCaracterBotao() { 
+    let textoAtual = resultado.value;
     let novoTexto = "";
     for (let index = 0; index < textoAtual.length - 1; index++) {
         novoTexto += textoAtual[index];   
     }
-    resultado.innerHTML = novoTexto || "0";
+    resultado.value = novoTexto || "0";
 }
 
 botoes.forEach((botao) => {
     botao.addEventListener("click", () => {
         const valor = botao.textContent;
-        if (valor == "C") {
-            resetarBotao()
+        if (valor === "C") {
+            resetarBotao();
             return;
-        } else if (valor == "CE") {
-            ApagarUmCaracterBotao();
         }
-        if (resultado.innerHTML == "0") {
-            resultado.innerHTML = valor;
+
+        if (valor === "CE") {
+            apagarUmCaracterBotao();
+            return;
+        }
+
+        if (valor == "=") {
+            const partes = resultado.value.split(operador);
+            parte1 = partes[0];
+            parte2 = partes[1];
+            calcular();
+            return;
+        }
+
+        if (operadoresDisponiveis.includes(valor) && operador == "") {
+            operador = valor;
+            resultado.value += valor;
+            return;
+        }
+
+        if (resultado.value == "0") {
+            resultado.value = valor
         } else {
-            resultado.innerHTML += valor
+            resultado.value += valor
         }
+        
     })
 });
+
+
+function calcular() {
+
+    let resultadoFinal = 0;
+    let numero1 = parseFloat(parte1);
+    let numero2 = parseFloat(parte2);
+
+    switch(operador) {
+        case "+": {
+            resultadoFinal = numero1 + numero2;
+            break;
+        }
+        case "-": {
+            resultadoFinal = numero1 - numero2;
+            break;
+        }
+        case "*": {
+            resultadoFinal = numero1 * numero2;
+            break;
+        }
+        case "÷": {
+            resultadoFinal = numero1 / numero2;
+            break;
+        }
+        case "%": {
+            resultadoFinal = numero1 + numero2;
+            break;
+        }
+    }
+    resultado.value = resultadoFinal;
+    operador = '';
+    numero1 = '';
+    numero2 = '';
+}
+
+
 
 
 
