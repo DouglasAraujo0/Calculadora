02d98b15acaecc4988cc32a75e2179295ef06e66
douglas <douglas.arau@hotmail.com>
2025-04-10 13:18:24 -0300

    botao de apagar apenas um caracter implementado

--- js/script.js
+++ js/script.js
@@ -2,9 +2,17 @@ const resultado = document.querySelector(".resultado");
 const botoes = document.querySelectorAll("button");
 let expressao = "";
 
-function botaoReset() {
+function resetarBotao() {
     resultado.textContent = "";
-} 
+}
+
+function ApagarUmCaracterBotao() { 
+    let novaExpressao = "";
+    for (let index = 0; index < expressao.length - 1; index++) {
+        novaExpressao[index] += expressao[index];   
+    }
+    expressao = novaExpressao;
+}
 
 
 botoes.forEach((botao) => {
